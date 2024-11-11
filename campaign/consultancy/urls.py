55ab3c6729bdf82daa587be5346696041d665727
finescribe83 <kristian.iroy@gmail.com>
2024-11-11 11:35:28 +0800

    Add files via upload

---
+++ campaign/consultancy/urls.py
@@ -0,0 +1,14 @@
+# urls.py
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),  # Ensure this line is correct
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
